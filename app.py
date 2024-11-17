7222718fcb47e2382aea6f5ac1d92150d877440d
40582229 <40582229@live.napier.ac.uk>
2024-11-17 21:03:31 +0000

    added more functionality to register stuff

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, g
 from flask_cors import CORS
+import json
 import sqlite3
+import bcrypt
 api = Flask(__name__)
 dbLocation = 'db/sqlite3.db'
 CORS(api)
@@ -39,6 +41,7 @@ def users():
     for row in db.cursor().execute(sql, (username,)):
         data.append(str(row))
 
+    db = closeDbConnection(exception=None)
     return data
 
 @api.route("/register", methods=['POST'])
@@ -46,10 +49,24 @@ def register():
     registerData = request.get_json()
     username = registerData['username']
     password = registerData['password']
+    if len(username) < 4 :
+        return json.dumps({"error":"Username must be atleast 4 character long"})
+    if len(password) < 8 :
+        return json.dumps({"error":"Password must be atleast 8 character long"})
     db = getDb()
+    sql_getUsersByUsername = "SELECT * FROM user WHERE username = ?"
+    
+    sqlRows_getUsersByUsernameLength = db.cursor().execute(sql_getUsersByUsername, (username,)).fetchall()
+    sqlRows_getUsersByUsernameLength = len(sqlRows_getUsersByUsernameLength)
+    if sqlRows_getUsersByUsernameLength>0:
+        return json.dumps({"error":"Username already taken"})
+
+    passwordHash = bcrypt.hashpw(password.encode ("utf -8 "), bcrypt.gensalt())
+    
     db.cursor().execute(
     'INSERT INTO user (username, password, token) VALUES (?, ?, ?)', 
-    (username, password, None))
+    (username, passwordHash, None))
+
     db.commit()
     db = closeDbConnection(exception=None)
-    return {"message":"success"}
+    return json.dumps({"success":"Account Created"})
